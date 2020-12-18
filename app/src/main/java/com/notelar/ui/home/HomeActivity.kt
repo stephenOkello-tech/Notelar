da3e80e5c02f9a3189eeed9506a8c19782c266a8
Okello Stephen <okellostephenomoding@gmail.com>
2020-12-18 20:11:36 +0300

    Initial Project Setup

---
+++ app/src/main/java/com/notelar/ui/home/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.notelar.ui.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.notelar.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
